2dd78bc2179064f67efb28dc5b64d2ef87bc35df
Danverde <gre12014@byui.edu>
2018-03-23 09:19:40 -0600

    added course code to backup course name

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const canvas = require('canvas-wrapper');
 module.exports = (course, stepCallback) => {
     function checkMigration(migration) {
         function check() {
-            canvas.get(`/api/v1/courses/${course.canvasOU}/blueprint_templates/default/migrations/${migration.id}`, (getErr, migrationDets) => {
+            canvas.get(`/api/v1/courses/${course.info.canvasOU}/blueprint_templates/default/migrations/${migration.id}`, (getErr, migrationDets) => {
                 if (getErr) {
                     course.fatalError(getErr);
                     stepCallback(getErr, course);
@@ -23,7 +23,7 @@ module.exports = (course, stepCallback) => {
                 } else if (migrationDets[0].workflow_state != 'completed') {
                     setTimeout(() => {
                         check();
-                    }, 1000 * 30);
+                    }, 1000 * 20);
                 } else {
                     stepCallback(null, course);
                 }
@@ -38,12 +38,13 @@ module.exports = (course, stepCallback) => {
             'copy_settings': true
         };
 
-        canvas.post(`/api/v1/courses/${course.canvasOU}/blueprint_templates/default/migrations`, postObj, (err, bpMigration) => {
+        canvas.post(`/api/v1/courses/${course.info.canvasOU}/blueprint_templates/default/migrations`, postObj, (err, bpMigration) => {
             if (err) {
                 course.fatalError(err);
                 stepCallback(err, course);
                 return;
             }
+            course.message('Starting to sync blueprint course');
             checkMigration(bpMigration);
         });
     }
@@ -54,15 +55,39 @@ module.exports = (course, stepCallback) => {
             'course_ids_to_add': course.info.prototypeOU
         };
 
-        canvas.put(`/api/v1/courses/${course.canvasOU}/blueprint_templates/default/update_associations`, putObj, (err) => {
+        canvas.put(`/api/v1/courses/${course.info.canvasOU}/blueprint_templates/default/update_associations`, putObj, (err) => {
             if (err) {
                 course.fatalError(err);
                 stepCallback(err, course);
                 return;
             }
+            course.message('Backup course associated with blueprint course');
             syncCourse();
         });
     }
 
-    associateCourse();
+    function createBackupCourse() {
+        
+        var courseObj = {
+            'course[name]': `${course.info.courseName} ${course.info.courseCode} Backup`,
+            'course[code]': course.info.courseCode
+        };
+
+        // WARNING where is the account ID stored?
+        canvas.post('/api/v1/accounts/19/courses', courseObj, (createErr, newCourse) => {
+            if (createErr) {
+                course.fatalError(createErr);
+                stepCallback(createErr, course);
+                return;
+            }
+            
+            course.message(`Backup course created with id: ${newCourse.id}`);
+            course.newInfo('prototypeOU', newCourse.id);
+            
+            associateCourse();
+        });
+    }
+
+    createBackupCourse();
+    // associateCourse();
 };
