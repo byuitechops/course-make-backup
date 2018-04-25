78aea46fffef9a48843560c08e6251613d1a79f2
Emma Fisher <emmarfisher99@gmail.com>
2018-04-25 14:02:54 -0600

    Adding Testing

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,4 +1,5 @@
 /*eslint-env node, es6*/
+/* eslint no-console:0 */
 
 /* Dependencies */
 const tap = require('tap');
@@ -6,61 +7,95 @@ const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
     tap.test('course-make-backup', (test) => {
-       // console.log('prototype', course);
+        // console.log('prototype', course);
+        console.log(course.settings);
 
-        canvas.get(`/api/v1/courses/${course.info.prototypeOU}`, (err, courseData) => {
-            if (err) {
-                course.error(err);
-                test.end();
-                return;
-            }
+        // canvas.get(`/api/v1/courses/${course.info.prototypeOU}`, (err, courseData) => {
+        //     if (err) {
+        //         course.error(err);
+        //         test.end();
+        //         return;
+        //     }
+        //     console.log(courseData);
 
-            canvas.get(`/api/v1/courses/${course.info.canvasOU}`, (err, courseDataOrig) => {
-                if (err) {
-                    course.error(err);
-                    test.end();
-                    return;
-                }
-                console.log('heya', courseDataOriginal);
-                
-                 // If the course exists in canvas
-                //test.ok(courseData, 'Course does not exist');
-                // If the course's title is the same as the main course                
-                //test.equal(courseData.name, courseDataOriginal.name);
-                
-            })    
-            
-        })
+        //     canvas.get(`/api/v1/courses/${course.info.canvasOU}`, (err, courseDataOrig) => {
+        //         if (err) {
+        //             course.error(err);
+        //             test.end();
+        //             return;
+        //         }
+        //         console.log('heya', courseDataOrig);
 
-        // If the course has content
-            // Number of pages
-             canvas.get(`/api/v1/courses/${course.info.prototypeOU}/pages`, (err, coursePages) => {
-                 if (err) {
-                     course.error(err);
-                     test.end();
-                     return;
-                 }
-                canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages`, (err, coursePagesOrig) => {
-                    if (err) {
-                        course.error(err);
-                        test.end();
-                        return;
-                    }
+        //         // If the course exists in canvas
+        //         test.ok(courseData, 'Course does not exist');
+        //         // If the course's title is the same as the main course                
+        //         test.equal(courseData.name, courseDataOrig.name);
 
-                    test.equal(coursePages.length, coursePagesOrig.length);
-                })
-                 
-             })
-            // Number of modules
-            // canvas.get(``)
-            // canvas.get(``)
-            // Number of quizzes
-            // canvas.get(``)
-            // canvas.get(``)
+        //     });
+
+        // });
+
+        // // If the course has content
+        // // Number of pages
+        // canvas.get(`/api/v1/courses/${course.info.prototypeOU}/pages`, (err, coursePages) => {
+        //     if (err) {
+        //         course.error(err);
+        //         test.end();
+        //         return;
+        //     }
+        //     canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages`, (err, coursePagesOrig) => {
+        //         if (err) {
+        //             course.error(err);
+        //             test.end();
+        //             return;
+        //         }
+
+        //         test.equal(coursePages.length, coursePagesOrig.length);
+        //     });
+
+        // });
+
+        // // Number of modules
+        // canvas.get(`/api/v1/courses/${course.info.prototypeOU}/modules`, (err, courseModules) => {
+        //     if (err) {
+        //         course.error(err);
+        //         test.end();
+        //         return;
+        //     }
+        //     canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules`, (err, courseModulesOrig) => {
+        //         if (err) {
+        //             course.error(err);
+        //             test.end();
+        //             return;
+        //         }
+
+        //         test.equal(courseModules.length, courseModulesOrig.length);
+        //     });
+
+        // });
+
+        // // Number of quizzes
+        // canvas.get(`/api/v1/courses/${course.info.prototypeOU}/quizzes`, (err, courseQuizzes) => {
+        //     if (err) {
+        //         course.error(err);
+        //         test.end();
+        //         return;
+        //     }
+        //     canvas.get(`/api/v1/courses/${course.info.canvasOU}/quizzes`, (err, courseQuizzesOrig) => {
+        //         if (err) {
+        //             course.error(err);
+        //             test.end();
+        //             return;
+        //         }
+
+        //         test.equal(courseQuizzes.length, courseQuizzesOrig.length);
+        //     });
+
+        // });
 
 
-    test.end();
+        test.end();
     });
 
     callback(null, course);
-}; 
+};
